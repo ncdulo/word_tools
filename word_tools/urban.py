dcafd0588c692d52d80ef28f9ba312587ca4d59e
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-03-15 17:45:44 -0400

    Initial revision `word_tools.urban`
    This function accepts a word and optional result limit argument.
    UrbanDictionary is queried using `requests` module, and the response is
    then parsed using `BeautifulSoup`. Returns a list of results up to
    `limit` results.
    
    Currently, `limit` can properly limit the number of results returned.
    But does not properly 'return all' when using the default of `0`.
    
    This can use some more clean up, but it works. Committing here as a
    functional checkpoint in case something goes horribly wrong with
    cleaning up and fixing `limit`.

---
+++ word_tools/urban.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+
+def lookup(word, limit=0):
+    '''
+    Return a list of definitions of 'word' from UrbanDictionary,
+    up to 'limit' results returned. Default 'limit' of 0 returns
+    all results.
+
+    Raises exception through 'requests' upon HTTP-related errors.
+    TODO: Document which exceptions
+
+    There is no safety-check on input. Use with caution. Can this even
+    be exploited? Figure that out.
+    '''
+    url = f'https://www.urbandictionary.com/define.php?term={word}'
+    response = requests.get(url)
+    result = []
+
+    # If we encounter a 4xx, or 5xx response code something went wrong
+    # Raise an exception. Let the caller determine how to handle it.
+    response.raise_for_status()
+
+    # Make some soup, look for meaning within ourself.
+    soup = BeautifulSoup(response.text, 'html.parser')
+    results = soup.find_all('div', class_='meaning')
+
+    # TODO: Limit does not work when set to 0
+    for index,meaning in enumerate(soup.find_all('div', class_='meaning')):
+        if index >= limit:
+            break
+        result.append(meaning.get_text())
+
+    # TODO: This is incorrect - fix.
+    print(result)
+    print(f'Returned {limit} results')
