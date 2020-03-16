2c6e017d6f68ed711b5057cc0abe89567f7a9ecc
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-03-16 19:34:57 -0400

    Create basic `setup.py` with single entry-point
    This is about the most basic, yet functional `setup.py` I had at the
    ready to try this alias-style entry-point out. Takes a bit of getting
    used to, but very powerful system. Very happy it was so easy to get the
    function aliases set up like this.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+        name='word_tools',
+        version='0.0.1',
+        py_modules=['cli'],
+        install_requires=[
+            'requests',
+            'click',
+        ],
+        entry_points = {
+                'console_scripts':
+                    ['wt.urban=word_tools.cli.cli:lookup_urban'],
+            }
+    )
