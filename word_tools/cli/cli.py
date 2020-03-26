0468cf8e099be37ff63c2c7f791602cdd11e6856
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-03-25 21:46:11 -0400

    Add `stoopid` entry-point
    Find a nice, stubby entry-point and attach it to `stoopid`. The `extra`
    argument is not properly accepted in this code, but it doesn't work in
    the main `stoopid` function either so there's no difference either way
    for the time being.
    
    This commit brings in the ability to `wt.stoopid` from the terminal,
    rather than firing up a REPL.

--- word_tools/cli/cli.py
+++ word_tools/cli/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from word_tools import lookup
+from word_tools import transform
 
 
 def display_results(results):
@@ -30,6 +31,13 @@ def lookup_urban(word, limit):
 def lookup_wikipedia(word, limit):
     results = lookup.wikipedia(word, limit)
     display_results(results)
+
+@click.command()
+@click.argument('word')
+# TODO: Properly implement extra-stoopid mode.
+#@click.argument('extra', default=None)
+def transform_stoopid(word):
+    print(transform.stoopid(word))
 
 
 def main():
