5c35bb9aca91d12497a28e6c70255b838809f438
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-03-26 17:36:23 -0400

    Typecheck `transform.stoopid` argument
    This fixes the failing `test_stoopid_bad_input` test. Very simple to
    implement, and could have done so easily last night. I was quite tired,
    and not thinking clearly. Figured it would be best left until after I
    could think it through some.
    
    After giving it some thought, I think the more proper way to test this
    is simply checking for a `str` type. This will reject anything that is
    *not* of the `str` type with a `TypeError` exception. The test passes,
    everyone is happy.
    
    Well, mostly happy. There is still one more test to fix. And one
    intermittent failure that also needs to be remedied.

--- word_tools/transform.py
+++ word_tools/transform.py
@@ -4,6 +4,10 @@ def stoopid(word, extra=None):
         argument to randomly flip case of a small proportion of letters
         after the first pass, for an 'EXtRa sTupID' final form.
     '''
+
+    if not isinstance(word, str):
+        raise TypeError(f'argument not recognized as string: {word}')
+
     new_word = ''
     for i, item in enumerate(word):
         if i % 2 == 0:
