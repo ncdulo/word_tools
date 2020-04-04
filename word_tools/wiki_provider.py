3d591fe4e9eaefbb40d9a68bd7c6092398ca097b
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-03 20:37:09 -0400

    Implement `WikiProvider`
    Add the new implementation of `WikiProvider` -- powered by
    `pymediawiki`! This module seems much nicer than the home-grown solution
    we previously used. In `__init__`, we set up a global `MediaWiki` object
    for this provider to use. And over in `lookup` we assemble a string
    featuring the title, url and trimmed summary before yielding each
    result.

--- word_tools/wiki_provider.py
+++ word_tools/wiki_provider.py
@@ -1,11 +1,21 @@
 from .lookup_provider import LookupProvider
 
+from mediawiki import MediaWiki
 
 class WikiProvider(LookupProvider):
     '''Concrete provider which provides web results from Wikipedia.
     '''
-    def lookup(self, word, limit=0):
-        return ['some', 'results', 'here']
+    def __init__(self):
+        self._wiki = MediaWiki(user_agent= \
+            "word_tools (https://github.com/ncdulo/word_tools")
+        LookupProvider.__init__(self)
+
+    def lookup(self, word, limit=1):
+        for result in self._wiki.opensearch(word, results=limit):
+            title, _, url = result
+            summary = self._wiki.page(title).summarize(chars=200)
+            output = title + ' (' + url + ')\n' + summary
+            yield output
 
 class WikiBuilder:
     '''Builder class which maintains a single instance of `WikiProvider`,
