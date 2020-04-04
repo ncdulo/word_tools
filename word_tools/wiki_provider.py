3a7413a320fe0d78585cede2bce910ec1fdb0dcb
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-03 20:52:19 -0400

    Hello Disambiguation, my old friend.
    Ahh, just couldn't get away from the `DisambiguationError` exception,
    could i? Turns out this is still a thing in this module as well.
    However, I have decided a proper way to handle the error this time
    around. If encountered, display a list of possible terms. The core issue
    seems like it may just be something related to Wikipedia itself. Need
    further investigation to say for sure.
    
    Also, set a default limit of three results when no argument is
    specified. `pymediawiki` handles the `results` argument quite literally.
    If `0`, or `None` are passed in you get just that.. No results. But you
    do get errors, if that's your thing. For the time being, circumvent by
    using a valid limit as the default.

--- word_tools/wiki_provider.py
+++ word_tools/wiki_provider.py
@@ -1,6 +1,6 @@
 from .lookup_provider import LookupProvider
 
-from mediawiki import MediaWiki
+from mediawiki import MediaWiki, exceptions
 
 class WikiProvider(LookupProvider):
     '''Concrete provider which provides web results from Wikipedia.
@@ -10,12 +10,25 @@ class WikiProvider(LookupProvider):
             "word_tools (https://github.com/ncdulo/word_tools")
         LookupProvider.__init__(self)
 
-    def lookup(self, word, limit=1):
-        for result in self._wiki.opensearch(word, results=limit):
-            title, _, url = result
-            summary = self._wiki.page(title).summarize(chars=200)
-            output = title + ' (' + url + ')\n' + summary
-            yield output
+    def lookup(self, word, limit=0):
+        # Default to a limit of three results. Once the re-write of CLI
+        # is complete, this should be updated, and likely removed
+        if limit == 0:
+            limit = 3
+
+        try:
+            for result in self._wiki.opensearch(word, results=limit):
+                title, _, url = result
+                summary = self._wiki.page(title).summarize(chars=200)
+                output = title + ' (' + url + ')\n' + summary
+                yield output
+        except exceptions.DisambiguationError as e:
+            print(
+                '''Search term disambiguous. There are some issues in the way
+results are returned. Wikipedia suggests the following page
+names. These may not be correct. This is a known issue.
+                ''')
+            print(e)
 
 class WikiBuilder:
     '''Builder class which maintains a single instance of `WikiProvider`,
