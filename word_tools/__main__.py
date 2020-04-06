c723a063aeaacc04d6768ef46a2bf11019341596
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-06 16:16:19 -0400

    Comments: Notes for improvement.
    I was considering to further continue tweaking the new CLI
    specification. Wrote some comments last night as notes for myself.
    Today, I am not sure I'm ready to tackle this bit just yet. Need some
    time to think through alternative solutions, and to determine if the one
    mentioned in these comments is a valid solution.
    
    I'll get back to this. For now, leaving the comments in as notes behind
    the madness here.

--- word_tools/__main__.py
+++ word_tools/__main__.py
@@ -20,6 +20,10 @@ def display_results(results):
 def main(provider, word, limit):
     # This can be done so much better, I'm sure. Have not determined
     # exactly how, just yet.
+
+    # TODO: I think we can do this better by placing all providers into
+    # `word_tools.providers.get(KEY)` and then using the actual provider's
+    # `__call__` method to perform the lookup/transform.
     try:
         main_prov = word_tools.lookup.get(provider)
         display_results(main_prov.lookup(word, limit))
